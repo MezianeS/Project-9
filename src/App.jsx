884939edca278b979a1a8a69af0b1a6fabac42f9
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-12-28 16:12:24 +0100

    Create & update navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,17 @@ import Navbar from './components/Components'
 function App() {
   return (
     <div className="w-full overflow-hidden">
-      <div className="home bg-primary">
-        <h1 className="text-4xl">Test.</h1>
-        <div className="">
-        <Navbar />
-        Home
-        Stats
-        Business
-        Agency
-        Footer
+      <div className="bg-primary">
+        <div className="home bg-primary flex justify-between p-5 px-10 mx-auto max-w-[1240px] items-center">
+          <h1 className="text-4xl text-[#f89c92] font-bold tracking-wide">FUTURE.</h1>
+          <Navbar />
         </div>
       </div>
+      Home
+      Stats
+      Business
+      Agency
+      Footer
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,29 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='navbar__projectnine'>
-        <nav>
-            <ul>
-                <li><a href="#home">Home</a></li>
-                <li><a href="#stats">Stats</a></li>
-                <li><a href="#business">Business</a></li>
-                <li><a href="#agency">Agency</a></li>
+    <div className='navbar__projectnine text-lg'>
+        <nav className='container flex'>
+            <ul className='hidden space-x-8 lg:flex'>
+                <li className='p-4'><a href="#stats">Home</a></li>
+                <li className='p-4'><a href="#stats">Stats</a></li>
+                <li className='p-4'><a href="#business">Business</a></li>
+                <li className='p-4'><a href="#agency">Agency</a></li>
             </ul>
+            <div className='lg:hidden'>
+                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
+                </svg>
+            </div>
+            <div className="fixed left-0 top-0 w-[60%] border-r border-r-gray-900">
+                <ul>
+                    <li className='p-4'><a href="#stats">Home</a></li>
+                    <li className='p-4'><a href="#stats">Stats</a></li>
+                    <li className='p-4'><a href="#business">Business</a></li>
+                    <li className='p-4'><a href="#agency">Agency</a></li>
+                </ul>
+            </div>
         </nav>
+        
     </div>
   )
 }
