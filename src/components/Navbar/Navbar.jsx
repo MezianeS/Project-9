4aa06226f860e5d4fc2172f90dc1ce456d8b39e0
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-12-29 17:10:07 +0100

    Update & add icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { AiOutlineClose } from 'react-icons/ai'
 
 
 const Navbar = () => {
@@ -17,16 +19,15 @@ const Navbar = () => {
                 <li className='p-4'><a href="#agency">Agency</a></li>
                 <li className='p-4'><a href="#contact">Contact</a></li>
             </ul>
-            <div className='lg:hidden' onClick={handleNav} > 
-                {/* <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
-                </svg> */}
-                <label for="check">
+            <div className='md:hidden' onClick={handleNav} > 
+                {/* <label for="check">
                     <input type="checkbox" id="check"/> 
                     <span></span>
                     <span></span>
                     <span></span>
-                </label>
+                </label> */}
+                {!nav ? <AiOutlineClose size={28}/> : <RxHamburgerMenu size={28}/> }
+                
             </div>
             <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#133C55] ease-in-out duration-500 max-[360px]:w-[70%]' : 'fixed left-[-100%]'}>
                 <h1 className="text-4xl text-[#f89c92] font-bold tracking-wide m-5 pl-5">FUTURE.</h1>
