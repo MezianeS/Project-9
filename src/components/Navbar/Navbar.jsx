0aae9a79000dfcb84177ba41177454589cd94bab
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2022-12-29 11:43:58 +0100

    Navbar update
    Fix menus & burger menus

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,19 +15,27 @@ const Navbar = () => {
                 <li className='p-4'><a href="#stats">Stats</a></li>
                 <li className='p-4'><a href="#business">Business</a></li>
                 <li className='p-4'><a href="#agency">Agency</a></li>
+                <li className='p-4'><a href="#contact">Contact</a></li>
             </ul>
             <div className='lg:hidden' onClick={handleNav} > 
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {/* <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
-                </svg>
+                </svg> */}
+                <label for="check">
+                    <input type="checkbox" id="check"/> 
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </label>
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#133C55] ease-in-out duration-500' : 'fixed left-[-100%]'}>
-                <h1 className="text-4xl text-[#f89c92] font-bold tracking-wide m-4">FUTURE.</h1>
+            <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#133C55] ease-in-out duration-500 max-[360px]:w-[70%]' : 'fixed left-[-100%]'}>
+                <h1 className="text-4xl text-[#f89c92] font-bold tracking-wide m-5 pl-5">FUTURE.</h1>
                     <ul className='p-4 uppercase'>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#stats">Home</a></li>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#stats">Stats</a></li>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#business">Business</a></li>
-                        <li className='p-4'><a href="#agency">Agency</a></li>
+                        <li className='p-4 border-b border-[#F89C92]'><a href="#agency">Agency</a></li>
+                        <li className='p-4'><a href="#contact">Contact</a></li>
                     </ul>
             </div>
         </nav>
