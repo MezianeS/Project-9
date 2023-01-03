2f8806243a34fd1f4f899b0d56bb16c32a38c765
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-03 13:49:40 +0100

    Update navbar
    Too muche !nav and this create issues

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,17 +19,17 @@ const Navbar = () => {
                 <li className='p-4'><a href="#agency">Agency</a></li>
                 <li className='p-4'><a href="#contact">Contact</a></li>
             </ul>
-            <div className='lg:hidden' onClick={handleNav} > 
+            <div className='lg:hidden block' onClick={handleNav} > 
                 {/* <label for="check">
                     <input type="checkbox" id="check"/> 
                     <span></span>
                     <span></span>
                     <span></span>
                 </label> */}
-                {!nav ? <AiOutlineClose size={28}/> : <RxHamburgerMenu size={28}/> }
+                {nav ? <AiOutlineClose size={28}/> : <RxHamburgerMenu size={28}/> }
                 
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#133C55] ease-in-out duration-500 max-[360px]:w-[70%]' : 'fixed left-[-100%]'}>
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#133C55] ease-in-out duration-500 max-[360px]:w-[70%]' : 'h-full ease-in-out duration-500 fixed left-[-100%]'}>
                 <img src="/../src/assets/logo.svg" alt="Logo Future." className='pnine__logo m-5'/>
                     <ul className='p-4 uppercase'>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#stats">Home</a></li>
