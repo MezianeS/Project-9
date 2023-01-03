e6a8b5ea8d3cfcc46fbd997f43c835c2b15d238a
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-03 16:18:40 +0100

    Fix img and text

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -4,9 +4,9 @@ import IMGhome from './../../assets/cyborgearth.png'
 
 const Index = () => {
   return (
-    <div className='container pnine__index--all p-6'>
+    <div className='container pnine__index--all p-6 lg:p-16'>
       {/* <h1 className="text-4xl text-[#f89c92] font-bold tracking-wide">FUTURE.</h1> */}
-      <div className='pnine__title--txt'>
+      <div className='pnine__title--txt lg:mt-12'>
         <h1 className='text-3xl md:text-4xl font-medium mt-10 mb-6 md:mt-20'>The world of technology at your fingertips</h1>
         <p>We believe that technology is an essential tool for innovation and progress, and we are eager to share our knowledge and enthusiasm with our online community.</p>
         <p>Join us and stay up to date on technological news!</p>
