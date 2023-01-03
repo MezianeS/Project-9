ba4cbb5d290341014e69d87364aa271bd357a029
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-03 15:58:14 +0100

    Add flexbox

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -6,11 +6,12 @@ const Index = () => {
   return (
     <div className='pnine__index--all p-6'>
       {/* <h1 className="text-4xl text-[#f89c92] font-bold tracking-wide">FUTURE.</h1> */}
-        <h1 className='text-3xl font-medium mt-10 mb-6'>The world of technology at your fingertips</h1>
+      <div className='pnine__title--txt'>
+        <h1 className='text-3xl md:text-4xl font-medium mt-10 mb-6 md:mt-20'>The world of technology at your fingertips</h1>
         <p>We believe that technology is an essential tool for innovation and progress, and we are eager to share our knowledge and enthusiasm with our online community.</p>
         <p>Join us and stay up to date on technological news!</p>
-        
-        <div className='p-0 bg-gradient-to-r from-transparent to-blue-500'>
+      </div>  
+        <div className='pnine__img--index'>
           <img src={IMGhome} alt="Bras robotique"/>
         </div>
 
