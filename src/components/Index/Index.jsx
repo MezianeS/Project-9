ee3621a729a266cf589eb8e31192e5d491a2c7b3
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-03 14:56:16 +0100

    English version

--- src/components/Index/Index.jsx
+++ src/components/Index/Index.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import IMGhome from './../../assets/cyborgearth.png'
 
 
 const Index = () => {
   return (
-    <div className='pnine__index--all'>
-
-
+    <div className='pnine__index--all p-6'>
       {/* <h1 className="text-4xl text-[#f89c92] font-bold tracking-wide">FUTURE.</h1> */}
-      <div>
-        <h1>Le monde de la technologie à portée de clic</h1>
-        <p>Nous croyons que la technologie est un outil essentiel pour l'innovation et le progrès, et nous sommes impatients de partager nos connaissances et notre enthousiasme avec notre communauté en ligne. Rejoignez-nous et restez à la pointe de l'actualité technologique !</p>
-        <img src="./../../assets/cyborgearth.png" alt="" />
-      </div>
+        <h1 className='text-3xl font-medium mt-10 mb-6'>The world of technology at your fingertips</h1>
+        <p>We believe that technology is an essential tool for innovation and progress, and we are eager to share our knowledge and enthusiasm with our online community.</p>
+        <p>Join us and stay up to date on technological news!</p>
+        <img src={IMGhome} alt="test" />
+
+
+        
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#0C2231',
+        primary: '#000000',
       },
     },
   },
