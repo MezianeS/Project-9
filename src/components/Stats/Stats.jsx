d4caa7bc5674e0e10562138bdcf9de92f42910ca
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-04 15:26:06 +0100

    Update Stats.jsx

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Stats = () => {
   return (
     <div className='pnine__stats--all'>
-        <div className="container__stats"></div>
+        <div className="container__stats">
+          <div className="container__stats--items bg-[#133C55]"></div>
+          <div className="container__stats--items bg-[#133C55]"></div>
+          <div className="container__stats--items bg-[#133C55]"></div>
+        </div>
+
         Stats
     </div>
   )
