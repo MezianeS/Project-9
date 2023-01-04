b0ed306223bb3b444c17a7a4944d2998603f5c79
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-04 11:48:52 +0100

    Update CTA.jsx

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -5,9 +5,9 @@ const CTA = () => {
   return (
     <div>
         <div className="container pnine__cta--all flex mt-28 justify-center">
-            <ImTwitter size={26} className="fill-blue-500 ml-2 mr-2" />
-            <FiInstagram size={26} className="fill-blue-500 ml-2 mr-2"/>
-            <ImFacebook size={26} className="fill-blue-500 ml-2 mr-2"/>
+            <ImTwitter size={26} className=" ml-3 mr-3" />
+            <FiInstagram size={26} className="ml-3 mr-3"/>
+            <ImFacebook size={26} className="ml-3 mr-3"/>
         </div>
     </div>
   )
