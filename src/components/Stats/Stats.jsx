0ec919757fae4c35e9596b8b7e4c9b50dd1d8669
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-04 17:08:52 +0100

    Add stats

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,10 +3,12 @@ import React from 'react'
 const Stats = () => {
   return (
     <div className='pnine__stats--all'>
-        <div className="container__stats">
-          <div className="container__stats--items bg-[#133C55]"></div>
-          <div className="container__stats--items bg-[#133C55]"></div>
-          <div className="container__stats--items bg-[#133C55]"></div>
+        {/* container pour le background */}
+        <div className="container__stats"></div>
+        <div className='flex flex-col lg:flex-row justify-center items-center'>
+          <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg ">1</div>
+          <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg">2</div>
+          <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg ">3</div>
         </div>
 
         Stats
