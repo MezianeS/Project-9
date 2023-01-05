b309c6691de5b47fa5f8b21284e2096efbd700c8
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-05 10:59:12 +0100

    Add new icons

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaBrain } from 'react-icons/fa'
+import IAicon from './../../assets/iaicon.png'
 
 const Stats = () => {
   return (
@@ -8,7 +9,8 @@ const Stats = () => {
         <div className="container__stats"></div>
         <div className='flex flex-col lg:flex-row justify-center items-center'>
           <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg ">
-          <FaBrain size={26} className="justify-center" />
+          <img src={IAicon} alt="Cerveau informatique" className='stats--items-1 p-5'/>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo adipisci doloribus dolor delectus numquam molestiae unde accusantium eveniet voluptatibus consectetur modi, aliquam tenetur fuga? Deleniti ullam accusamus rem iusto optio!</p>
           </div>
           <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg">2</div>
           <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg ">3</div>
