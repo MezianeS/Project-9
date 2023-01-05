565f11f6d7c386fb7057ea7ec519d64b0edc5e1d
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-05 12:43:18 +0100

    Update Stats.jsx

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -7,14 +7,23 @@ const Stats = () => {
     <div className='pnine__stats--all'>
         {/* container pour le background */}
         <div className="container__stats"></div>
-        <div className='flex flex-col lg:flex-row justify-center items-center'>
+        {/* container pour le background */}
+        <div className='flex flex-col lg:flex-row justify-center items-center mt-10'>
           <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg flex flex-col justify-center items-center">
           <img src={IAicon} alt="Cerveau informatique" className='stats--items-1 pl-3 pr-3 pt-5'/>
           <h4 className='pb-1 pt-2 font-bold text-[#91E5F6]'>AI:Artifical Intelligence</h4>
-          <p className='pl-5 pr-5 pb-2 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo adipisci doloribus dolor delectus numquam.</p>
+          <p className='pl-5 pr-5 pb-5 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo adipisci doloribus dolor delectus numquam.</p>
           </div>
-          <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg">2</div>
-          <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg ">3</div>
+          <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg flex flex-col justify-center items-center">
+          <img src={IAicon} alt="Cerveau informatique" className='stats--items-1 pl-3 pr-3 pt-5'/>
+          <h4 className='pb-1 pt-2 font-bold text-[#91E5F6]'>AI:Artifical Intelligence</h4>
+          <p className='pl-5 pr-5 pb-5 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo adipisci doloribus dolor delectus numquam.</p>
+          </div>
+          <div className="container__stats--items bg-[#0C2231] m-5 border-solid border-2 rounded-lg flex flex-col justify-center items-center">
+          <img src={IAicon} alt="Cerveau informatique" className='stats--items-1 pl-3 pr-3 pt-5'/>
+          <h4 className='pb-1 pt-2 font-bold text-[#91E5F6]'>AI:Artifical Intelligence</h4>
+          <p className='pl-5 pr-5 pb-5 text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Explicabo adipisci doloribus dolor delectus numquam.</p>
+          </div>
         </div>
 
         Stats
