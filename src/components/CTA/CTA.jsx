c89a4d91d9ed64569dcb1c662d8959b034e17388
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-09 12:59:52 +0100

    Update CTA.jsx

--- src/components/CTA/CTA.jsx
+++ src/components/CTA/CTA.jsx
@@ -4,7 +4,7 @@ import { FiInstagram } from 'react-icons/fi'
 const CTA = () => {
   return (
     <div>
-        <div className="container pnine__cta--all flex mt-28 justify-center">
+        <div className="container pnine__cta--all flex mt-28 lg:mt-14 justify-center">
             <a href="" className='text-[#0C2231]'><ImTwitter size={22} className="cta--icons" /></a>
             <a href=""className='text-[#0C2231]'><FiInstagram size={22} className="cta--icons "/></a>
             <a href="" className='text-[#0C2231]'><ImFacebook size={22} className="cta--icons"/></a>
