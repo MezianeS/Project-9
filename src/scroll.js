d9cbe93ecf4655e151e199e0e3158c2c3c0875bd
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-10 17:03:23 +0100

    Update scroll.js

--- src/scroll.js
+++ src/scroll.js
@@ -1,16 +1,1 @@
-export default function Scroll() {
-    // function setup() {
-    //     createCanvas(600, 600);
-    // }
 
-    // function draw() {
-    //     background(0);
-    // }
-
-    const observer = new IntersectionObserver((entries) => {
-        entries.forEach((entry) => {
-            console.log(entry)
-            if (entry.isIntersecting) {
-                entry.target.classList.add('show');
-            } else {
- 
