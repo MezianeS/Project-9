c8364642cd781be61687fd63ae2dfe6d6a73d293
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-10 17:03:14 +0100

    Update scroll.js

--- src/scroll.js
+++ src/scroll.js
@@ -13,11 +13,4 @@ export default function Scroll() {
             if (entry.isIntersecting) {
                 entry.target.classList.add('show');
             } else {
-                entry.target.classList.remove('show');
-            }
-        });
-    });
-
-    const hiddenElements = document.querySelectorAll('.hiddenscroll');
-    hiddenElements.forEach((el) => observer.observe(el));
-}
+ 
