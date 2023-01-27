324758d6a5a05d9895baf93cad349b2fdefff90c
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-27 11:18:37 +0100

    Update Business.jsx

--- src/components/Business/Business.jsx
+++ src/components/Business/Business.jsx
@@ -6,7 +6,7 @@ const Business = () => {
     <div className='business__container bg-[black]'>
         <div className="business__container--block mx-auto max-w-[1240px] items-center p-6'">
             <div className="business__container--text">
-                <h2 className="business__container-title">BUSINESS</h2>
+                <h2 className="business__container-title text-center pt-24 text-3xl md:text-4xl lg:text-5xl font-medium">BUSINESS</h2>
                 <p className="business__container--textblock"></p>            
             </div>
             <img src={PlanetsImg} alt="" className='business__content--img' />
