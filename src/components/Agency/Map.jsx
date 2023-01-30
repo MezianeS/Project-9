bd89168fa59b3aa279476f5a1e6ddfbf8fca0d2a
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-30 16:23:31 +0100

    Update Map.jsx

--- src/components/Agency/Map.jsx
+++ src/components/Agency/Map.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Map
  = () => {
   return (
-    <div>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5249.602880760883!2d2.3285957829600967!3d48.861996600000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2f3703339d%3A0xd6fa960a6ce2083a!2sJardin%20du%20Carrousel!5e0!3m2!1sfr!2sfr!4v1675088555015!5m2!1sfr!2sfr" width={400} height={300} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>        
+    <div className='mt-10'>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.8019434053886!2d2.3294062158910394!3d48.861987008407155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2f2faf1899%3A0x4041baaa9cfa6c2c!2s7%20Av.%20du%20G%C3%A9n%C3%A9ral%20Lemonnier%2C%2075001%20Paris!5e0!3m2!1sfr!2sfr!4v1675089538011!5m2!1sfr!2sfr" width={600} height={450} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>        
     </div>
   )
 }
