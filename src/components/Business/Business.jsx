68e9380a24f7e9a30a9a0ce252c6eee571531cb4
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-30 14:33:38 +0100

    Update Business.jsx

--- src/components/Business/Business.jsx
+++ src/components/Business/Business.jsx
@@ -5,7 +5,7 @@ const Business = () => {
   return (
     <div className='business__container bg-[black] pb-24'>
         <div className="business__container--block mx-auto max-w-[1240px] items-center p-6'">
-            <h2 className="business__container-title text-center pt-24 lg:pb-16 text-3xl md:text-4xl lg:text-5xl font-medium ">BUSINESS</h2>
+            <h2 className="business__container-title text-center pt-24 lg:pb-16 text-3xl md:text-4xl lg:text-5xl font-medium " id="business">BUSINESS</h2>
             <div className="business__container--textimg flex flex-col lg:flex-row">
                 <p className="business__container--textblock mt-10 m-3 lg:ml-48 lg:mr-48 p-10 lg:pl-0 text-center lg:text-start text-lg">
                 <span className='text-2xl font-bold text-[#F89C92]'>Define your experience</span>
