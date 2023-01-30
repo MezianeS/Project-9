9d3e8d4f61a4807283f6cc506b9671b713c3f6e3
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-01-30 15:29:41 +0100

    Create a map files

--- src/components/Agency/Agency.jsx
+++ src/components/Agency/Agency.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Map from './Map'
+
 
 const Agency = () => {
   return (
     
     <div className='agency__container pb-24'>
         <div className="agency__container--block mx-auto max-w-[1240px] items-center p-6'">
-            <div>Test</div>
+            <h2 className='agency__container--title text-[black] text-center pt-24 lg:pb-16 text-3xl md:text-4xl lg:text-5xl font-medium '>AGENCY</h2>
+            <div className="agency__container--carte">
+                <Map />
+            </div>
         </div>
     </div>
   )
---
+++ src/components/Agency/Map.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Map
+ = () => {
+  return (
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5249.602880760883!2d2.3285957829600967!3d48.861996600000005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2f3703339d%3A0xd6fa960a6ce2083a!2sJardin%20du%20Carrousel!5e0!3m2!1sfr!2sfr!4v1675088555015!5m2!1sfr!2sfr" width={400} height={300} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>        
+    </div>
+  )
+}
+
+export default Map
