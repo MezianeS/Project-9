39666f75a350a2af410cdba125e0beaeacc8f6f2
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-02-02 17:09:51 +0100

    Update CSS

--- src/components/Agency/Agency.jsx
+++ src/components/Agency/Agency.jsx
@@ -8,7 +8,7 @@ const Agency = () => {
     <div className='agency__container pb-24'>
         <div className="agency__container--block mx-auto max-w-[1240px] items-center p-6'">
             <h2 className='agency__container--title text-[black] text-center pt-24 lg:pb-16 text-3xl md:text-4xl lg:text-5xl font-medium '>AGENCY</h2>
-            <div className="agency__container--carte">
+            <div className="agency__container--carte mt-10">
                 <Map />
             </div>
         </div>
