d35fcc7e4c9d021ade7a0c7cf235390a897502c8
SosoMeziane <89013262+MezianeS@users.noreply.github.com>
2023-02-15 16:56:50 +0100

    update css

--- src/components/Agency/Map.jsx
+++ src/components/Agency/Map.jsx
@@ -10,6 +10,8 @@ const Map
               7 Av. du General Lemmonier
               <br />
               75001, Paris
+              <br />
+              Contact us: +852 756 123 898
             </div>
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2624.8019434053886!2d2.3294062158910394!3d48.861987008407155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e2f2faf1899%3A0x4041baaa9cfa6c2c!2s7%20Av.%20du%20G%C3%A9n%C3%A9ral%20Lemonnier%2C%2075001%20Paris!5e0!3m2!1sfr!2sfr!4v1675089538011!5m2!1sfr!2sfr" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade">
           </iframe>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     }
   return (
     <div className='navbar__projectnine text-lg'>
-        <nav className='container flex'>
+        <div className='container flex'>
             <ul className='hidden space-x-8 lg:flex hiddenscroll'>
                 <li className='p-4'><a href="#">Home</a></li>
                 <li className='p-4'><a href="#services">Services</a></li>
@@ -20,27 +20,17 @@ const Navbar = () => {
                 <li className='p-4'><a href="#contact">Contact</a></li>
             </ul>
             <div className='lg:hidden block' onClick={handleNav} > 
-                {/* <label for="check">
-                    <input type="checkbox" id="check"/> 
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </label> */}
                 {nav ? <AiOutlineClose size={28}/> : <RxHamburgerMenu size={28}/> }
-                
             </div>
-            <div className={nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#133C55] ease-in-out duration-500 max-[360px]:w-[70%]' : 'h-full ease-in-out duration-500 fixed left-[-100%]'}>
-                <img src="/../src/assets/logo.svg" alt="Logo Future." className='pnine__logo m-5'/>
-                    <ul className='p-4 uppercase'>
+            <ul className={nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#133C55] ease-in-out duration-500 max-[360px]:w-[70%]' : 'h-full ease-in-out duration-500 fixed left-[-100%]'}>
+                        <img src="/../src/assets/logo.svg" alt="Logo Future." className='pnine__logo m-5'/>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#">Home</a></li>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#services">Services</a></li>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#business">Business</a></li>
                         <li className='p-4 border-b border-[#F89C92]'><a href="#agency">Agency</a></li>
                         <li className='p-4'><a href="#contact">Contact</a></li>
                     </ul>
-            </div>
-        </nav>
-        
+        </div>
     </div>
   )
 }
